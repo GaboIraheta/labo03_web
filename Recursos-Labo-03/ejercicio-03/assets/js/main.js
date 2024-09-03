38c7aa4f75eac66f56f442824b1aeb56544410db
GaboIraheta <00021223@uca.edu.sv>
2024-09-03 10:35:43 -0600

    :white_check_mark: Ejercicio 03 realizado correctamente

--- Recursos-Labo-03/ejercicio-03/assets/js/main.js
+++ Recursos-Labo-03/ejercicio-03/assets/js/main.js
@@ -1,13 +1,35 @@
-const calculateFactorial = (/*recibe*/) => {
+const calculateFactorial = (number) => {
     //Code
+    let fact = 1;
+
+    for (let i = 1 ; i <= number ; i++) {
+        fact *= i;
+    }
+
+    return fact;
 }
 
 const requestNumber = () => {
     //Code
+    let num = prompt("Ingresa un numero entero no negativo: ");
+
+    if((!isNaN(num)) && (num >= 0)) {
+        return num;
+    } return false;
 }
 
 const main = () => {
     //Code
+    let number = requestNumber();
+
+    if (number === false) {
+        alert("El valor ingresado no es valido (debe ser entero no negativo)");
+        return;
+    }
+
+    let fact = calculateFactorial(number);
+
+    alert(`El factorial de ${number} es: ${fact}`);
 }
 
 main();
